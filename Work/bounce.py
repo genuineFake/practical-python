af2d35fd020c9add1364f103231e36b937c63043
genuineFake <batraistis@gmail.com>
2021-02-25 16:53:33 +0200

    1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_back = 0.60
+count = 1
+
+while count <= 10:
+    print(count, round(height * bounce_back, 4))
+    height *= bounce_back
+    count += 1
