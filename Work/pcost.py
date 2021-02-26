4958a8c9c5aa4bab5ed741452c77fde04a87cd87
genuineFake <batraistis@gmail.com>
2021-02-26 18:39:39 +0200

    02.26

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open('Data/portfolio.csv', 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                total_cost += float(row[1]) * float(row[2])
+            except ValueError:
+                print("Couldn't parse", shares)
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost ', cost)
