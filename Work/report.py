af694dc05f5d0c57a969f486154833bceec1046c
genuineFake <batraistis@gmail.com>
2021-02-28 17:37:55 +0200

    02.28

--- Work/report.py
+++ Work/report.py
@@ -27,14 +27,22 @@ def read_prices(filename):
 
     return prices
 
+
+# Read data files and create the report data
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices    = read_prices('Data/prices.csv')
 
+
+#Calculate total cost of portfolio
 
 portfolio_cost = 0.0
 for s in portfolio:
     portfolio_cost += s['shares'] * s['price']
 print('Value of the portfolio ', portfolio_cost)
+
+
+#Calculate current value of the portfolio
 
 portfolio_value = 0.0
 for s in portfolio:
@@ -42,3 +50,26 @@ for s in portfolio:
 
 print('Current value', portfolio_value)
 print('Gain', portfolio_value - portfolio_cost)
+
+
+#Takes a list of stocks and dictionary of prices as input and returns a list of tuples
+
+def make_report(portfolio, prices):
+    report = []
+    for shares in portfolio:
+        current_price = prices[shares['name']]
+        change = current_price - shares['price']
+        current_portfolio = (shares['name'], shares['shares'], current_price, change)
+        report.append(current_portfolio)
+    return report
+
+report = make_report(portfolio, prices)
+
+
+#Prints a nicely formatted table
+
+def print_report(report):
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
+
+print_report(report)
